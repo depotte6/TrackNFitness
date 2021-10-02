4dc14283d9491a36b8ffc4c7b8e12ac9e98c031f
depotte6 <depotte6@gmail.com>
2021-10-02 18:45:02 -0400

    added models

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require("morgan")
+const logger = require("morgan");
+const path = require("path");
+const apiRoutes = require("./routes/api-routes");
+const htmlRoutes = require("./routes/html-routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,6 +18,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 
 app.listen(PORT, () => {
