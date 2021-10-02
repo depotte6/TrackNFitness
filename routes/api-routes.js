831f9c9cb5fd8a68bec3a67ad9ae2590493b934b
depotte6 <depotte6@gmail.com>
2021-10-02 18:47:43 -0400

    server typo

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const db = require("../models");
 require("mongoose");
 
-router.length("/api/workouts", (req, res) => {
+router.get("/api/workouts", (req, res) => {
     db.find()
     .then(workouts => res.json(workouts))
     .catch(err => res.json(err));
