3029cf4ee708b0837421a7628e799a6e5ab5d023
depotte6 <depotte6@gmail.com>
2021-10-02 21:38:16 -0400

    route troubleshooting

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,52 @@
 const router = require("express").Router();
-const db = require("../models");
+const db = require("../models/workout");
 require("mongoose");
 
+//gets all past workouts
 router.get("/api/workouts", (req, res) => {
-    db.find()
-    .then(workouts => res.json(workouts))
-    .catch(err => res.json(err));
+	db.find()
+		.then((dbData) => {
+			res.json(dbData);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
 });
+
+//get last 7 workouts
+router.get("/api/workouts/range", (req, res) => {
+    db.find({})
+    .then(dbData => {
+        res.json(dbData);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
+
+//add workout
+router.post("/api/workouts", (req, res) => {
+    db.create({
+      day: Date.now()
+    })
+      .then(newWorkout => {
+        console.log("New Workout Created: ", newWorkout);
+        res.json(newWorkout);
+      })
+      .catch(err => res.json(err));
+  });
+  
+
+  router.put("/api/workouts/:id", (req, res) => {
+    db.findByIdAndUpdate(
+      req.params.id,
+      { $push: { exercises: req.body } },
+      { new: true }
+    )
+      .then(workout => res.json(workout))
+      .catch(err => res.json(err));
+  });
+  
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -16,9 +16,10 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
+app.use(logger("dev"));
 app.use(apiRoutes);
 app.use(htmlRoutes);
 
